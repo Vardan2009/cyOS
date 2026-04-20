10049d589db0622786a12c12b2ace30b0731953c
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2026-04-20 18:21:57 +0400

    heapSize update

--- mm/kmalloc.c
+++ mm/kmalloc.c
@@ -1,5 +1,7 @@
 #include "kmalloc.h"
 
+#include <stdio.h>
+
 #include "math.h"
 #include "memory.h"
 
@@ -11,13 +13,17 @@ void ChangeHeapSize(int newSize) {
     int oldPageTop = CEIL_DIV(heapSize, 0x1000);
     int newPageTop = CEIL_DIV(newSize, 0x1000);
 
-    int diff = newPageTop - oldPageTop;
+    if (newPageTop > oldPageTop) {
+        int diff = newPageTop - oldPageTop;
 
-    for (int i = 0; i < diff; ++i) {
-        uint32_t phys = PMMAllocPageFrame();
-        MemMapPage(KERNEL_MALLOC + oldPageTop * 0x1000 + i * 0x1000, phys,
-                   PAGE_FLAG_WRITE);
+        for (int i = 0; i < diff; ++i) {
+            uint32_t phys = PMMAllocPageFrame();
+            MemMapPage(KERNEL_MALLOC + oldPageTop * 0x1000 + i * 0x1000, phys,
+                       PAGE_FLAG_WRITE);
+        }
     }
+
+    heapSize = newSize;
 }
 
 void KMallocInit(uint32_t initialHeapSize) {
@@ -27,4 +33,7 @@ void KMallocInit(uint32_t initialHeapSize) {
     kmallocInitialized = 1;
 
     ChangeHeapSize(initialHeapSize);
+    *((uint32_t *)heapStart) = 0;
+
+    printf("Heap start: %x\nHeap size: %x\n", heapStart, heapSize);
 }
